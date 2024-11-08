c949767f52180840fec5bcdcc0578d90ee439e98
faizanKhan <faizankhan46849@gmail.com>
2024-11-08 15:28:40 +0530

    Add Reset butoon

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,5 @@
 import Button from '@mui/material/Button';
+import { CiRedo } from "react-icons/ci";
 import { useState } from 'react';
 
 export default function Counter () {
@@ -13,9 +14,14 @@ export default function Counter () {
     setCount( (prevCount) => prevCount - 1);
    }
 
+   let reset = () => {
+     setCount( (prevCount) => prevCount = 0);
+   }
+
     return (
         <div className=' flex justify-center items-center h-[100vh]'>
-           <div className=' border-[2px] rounded-md border-black w-96 h-56 text-center pt-8 bg-red-400'>
+           <div className=' border-[2px] rounded-md border-black w-96 h-56 text-center pt-3 bg-red-400'>
+             <div className=' flex justify-end text-[30px] font-medium pr-4 items-center pt-2  cursor-pointer '><span className='text-[18px] pr-1'>Reset</span> <CiRedo onClick={reset} /></div>
              <h1 className='text-[30px] font-medium'>Counter App</h1>
              <h3 className='text-[18px] mb-4'>Count = {count}</h3>
              <div className="flex gap-3 justify-center">
